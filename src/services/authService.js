a98b8b9d934719e33364c67cc7fa6cda998ffdce
lpapantoniou <lpapantoniou@knowledge.gr>
2024-01-15 13:38:55 +0200

    implementation of auth service

---
+++ src/services/authService.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
+const {PrismaClient} = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const authService = {
+    async registerUser(email, password) {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Add user to the database
+        const newUser = await prisma.users.create({
+            data: {
+                email: email,
+                password: hashedPassword
+            }
+        });
+
+        // Exclude password from the returned user object
+        const { password: _, ...userWithoutPassword } = newUser;
+
+        return userWithoutPassword;
+    },
+
+    async loginUser(email, password) {
+        // Check if user exists
+        const user = await prisma.users.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        // Check if incoming password is the same as the database password
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            throw new Error('Invalid credentials');
+        }
+
+        // Generate a JWT token
+        const token = jwtGenerator(user.id);
+
+        return { user, token };
+    },
+
+};
+
+module.exports = authService;
