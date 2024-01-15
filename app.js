d6e43ab473d8b583030a4b714efbf3260617aedc
lpapantoniou <lpapantoniou@knowledge.gr>
2024-01-15 13:41:05 +0200

    addition of chat-Gpt routs to main app

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const cors = require("cors");
 const authRoutes = require('./src/routes/authRoutes');
+const chatGptRoutes = require('./src/routes/chatGptRoutes');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(cors());
 
 // Use the authentication routes
 app.use('/api/user', authRoutes);
+
+// Use the Chat-Gpt routes
+app.use('/api', chatGptRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
