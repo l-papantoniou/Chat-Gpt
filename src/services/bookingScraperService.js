4d0078ad023c95b4707c4337b0d3ba22827aef16
lpapantoniou <lpapantoniou@knowledge.gr>
2024-01-28 19:44:17 +0200

    update the scrapping service implementation

--- src/services/bookingScraperService.js
+++ src/services/bookingScraperService.js
@@ -10,8 +10,8 @@ const bookingScraperService = {
     scrapBookingData: async (booking_url) => {
         // Launch Puppeteer
         const browser = await puppeteer.launch({
-            headless: false,
-            args: ['--start-maximized'],
+            headless: "new",
+            args: ['--start-minimized'],
             defaultViewport: null
         })
 
@@ -32,15 +32,18 @@ const bookingScraperService = {
 
         })
 
+
         // Extract hotel description
 
         const description = await page.evaluate(() => {
 
-            const description = document.querySelectorAll('div[id="property_description_content"]')
+            // Use a different variable name for the querySelectorAll result
+            const descriptionElements = document.querySelectorAll('div[id="property_description_content"]');
 
-            // Check if there are any description and return the first one's text content
-            return description.length > 0 ? description[0].textContent : null;
-        })
+            // Check if there are any description elements and return the first one's text content
+            return descriptionElements.length > 0 ? descriptionElements[0].textContent : null;
+        });
+
 
         // Extract hotel location
 
@@ -53,56 +56,29 @@ const bookingScraperService = {
         });
 
 
-        // Extract amenities and services
+        // Extract assets and amenities
 
-        const highlights = await page.evaluate(() => {
+        const assets = await page.evaluate(() => {
             // Select the element using one of its classes
-            const highlights = document.querySelectorAll('div[class="property-highlights ph-icon-fill-color"]');
+            const assets = document.querySelectorAll('div[data-testid="property-most-popular-facilities-wrapper"] li');
 
-            // Return the text content of the element
-            return highlights.length > 0 ? highlights[0].textContent.trim() : null;
+            // Extract the text content of each amenity and remove duplicates
+            const assetslist = Array.from(new Set(Array.from(assets).map(el => el.textContent.trim())));
+
+            return assetslist; // Return the array of amenities
         });
 
+        const type = await page.evaluate((description) => {
+            const hotelTypes = ["hotel", "house", "suites", "boutique", "resort", "hostel", "inn", "B&B", "luxury", "budget", "apartment"];
 
-        // Extract amenities and services
+            let type = hotelTypes.find(type => description.toLowerCase().includes(type));
 
-        const amenities = await page.evaluate(() => {
-            // Select the element using one of its classes
-            const amenities = document.querySelectorAll('div[data-testid="property-most-popular-facilities-wrapper"]');
-
-
-            // Return the text content of the element
-            return amenities.length > 0 ? amenities[0].textContent : null;
-        });
-
-        const type = await page.evaluate(() => {
-
-            const hotelTypes = ["hotel", "boutique", "resort", "hostel", "inn", "B&B", "luxury", "budget", "apartment"];
-
-            let types = hotelTypes.filter(type => description.toLowerCase().includes(type));
-
-            return type.length > 0 ? type : null
-
-        });
-
-
-        // // Extract amenities and services
-        //
-        // const amenities = await page.evaluate(() => {
-        //     let results = [];
-        //     // Example: extract amenities from a specific section
-        //     document.querySelectorAll('.e1eebb6a1e e6208ee469').forEach((section) => {
-        //         const categoryName = section.querySelector('.d1ca9115fe').innerText;
-        //         const items = Array.from(section.querySelectorAll('.a53cbfa6de e6208ee469 .a5a5a75131')).map(item => item.innerText);
-        //         results.push({category: categoryName, items: items});
-        //     });
-        //     return results;
-        // });
-
+            return type || null; // Return the type or null if none is found
+        }, description); // Pass description as an argument here
 
         await browser.close()
 
-        return {name, description, location, highlights, amenities, type};
+        return {name, description, location, type, assets};
     }
 
 }
